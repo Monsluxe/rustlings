c86687bda9cd23dd88b55171c75dc39d21cf19f8
MONSLUXE <simon.constant286@gmail.com>
2024-01-20 21:01:08 +0100

    done strings and module modules

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -8,10 +8,10 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // TODO: Complete this use statement
-use ???
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
@@ -19,3 +19,6 @@ fn main() {
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
 }
+
+
+//Missing a use statement (thx to the hint), now SystemTime and Unix EPOCH are in scope, they are now available for the main 
