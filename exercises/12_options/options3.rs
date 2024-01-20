f0883ea75324045511310027ad8b0d74a234e159
MONSLUXE <simon.constant286@gmail.com>
2024-01-20 21:52:16 +0100

    done options and quizz modules

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct Point {
     x: i32,
@@ -14,8 +14,12 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _ => println!("no match"),
     }
     y; // Fix without deleting this line.
 }
+
+
+//WRITEUP 
+//In the script, basically the y var was moved into match function, making it inacessible after that, i just created another variable that stay more global, and store the var data of y variable 
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -31,12 +30,23 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+        for (mut string, command) in input {
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                Command::Append(times) => {
+                    for _ in 0..times {
+                        string.push_str("bar");
+                    }
+                    output.push(string);
+                }
+            }
         }
         output
     }
@@ -44,17 +54,16 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer; // Fixed import statement
     use super::Command;
 
     #[test]
     fn it_works() {
         let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
         ]);
         assert_eq!(output[0], "HELLO");
         assert_eq!(output[1], "all roads lead to rome!");
