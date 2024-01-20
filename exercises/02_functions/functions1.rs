f0a63eed33b741088c88da2d2a81f65f3cf0d055
MONSLUXE <simon.constant286@gmail.com>
2024-01-20 11:57:40 +0100

    done firsts two function exercices

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,15 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+fn call_me() {
+    println!("call from function call_me()!");
+}
 
 fn main() {
     call_me();
 }
+
+//WRITEUP : 
+//Here the callme function is called but not initializd, so lets create the function.
--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -3,14 +3,15 @@
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
+fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
 }
+
+//On ajoute le paramètre num à la fonction call_me, avec le type de donnée qui va avec (i32 pour une int en 32bits)
+//Le print est deja correctement géré, donc une fois la signature de call_me complete, l'output est correct, le code compile
